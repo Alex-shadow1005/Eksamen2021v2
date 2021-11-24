ef61362b3c831a82aaf15938be5c66fe48ec338e
Silke Marie <silk2918@stud.kea.dk>
2021-11-24 11:14:04 +0100

    Kristian + Silke createProject + create SubProject + showall + showallsubprojects metoder

--- src/main/java/com/example/eksamen2021/repositories/ProjectRepository.java
+++ src/main/java/com/example/eksamen2021/repositories/ProjectRepository.java
@@ -1,6 +1,7 @@
 package com.example.eksamen2021.repositories;
 
 import com.example.eksamen2021.domain.models.Project;
+import com.example.eksamen2021.domain.models.SubProject;
 import com.example.eksamen2021.domain.models.User;
 
 import java.sql.Connection;
@@ -12,7 +13,7 @@ import java.util.List;
 
 public class ProjectRepository {
 
-  //TILFØJER ET ØNSKE TIL BRUGERENS ØNSKELISTE
+  //TILFØJER ET PROJEKT TIL BRUGERENS PROJEKTLISTE
   public int addProject(Project project, User user) {
     String mySql;
     PreparedStatement ps;
@@ -22,11 +23,11 @@ public class ProjectRepository {
       //1. Get a connection to database
       Connection con = DBManager.getConnection();
       //2. Prepare statement
-      mySql = "INSERT INTO wishwishwish.wishes (user_id, project_name, project_description) VALUES (?, ?, ?)";
+      mySql = "INSERT INTO projects (project_id, project_name, project_description) VALUES (?, ?, ?)";
 
       ps = con.prepareStatement(mySql);
       //3. Set the parameters
-      ps.setInt(1, user.getUser_id());
+      ps.setInt(1, project.getProject_id());
       ps.setString(2, project.getProject_name());
       ps.setString(3, project.getProject_description());
       //4. Execute SQL query
@@ -39,12 +40,39 @@ public class ProjectRepository {
     return h; //returnerer brugeren til Service
   }
 
+  //TILFØJER ET SUBPROJEKT TIL BRUGERENS SUBPROJEKTLISTE
+  public int addSubProject(SubProject subProject, User user) {
+    String mySql;
+    PreparedStatement ps;
+    int h = 0;
+
+    try {
+      //1. Get a connection to database
+      Connection con = DBManager.getConnection();
+      //2. Prepare statement
+      mySql = "INSERT INTO subprojects (subproject_id, subproject_name, subproject_description) VALUES (?, ?, ?)";
+
+      ps = con.prepareStatement(mySql);
+      //3. Set the parameters
+      ps.setInt(1, subproject.getSubproject_id());
+      ps.setString(2, subproject.getSubproject_name());
+      ps.setString(3, subproject.getSubproject_description());
+      //4. Execute SQL query
+      h = ps.executeUpdate();
+      System.out.println("Subproject added");
+      //5. Display the result set
+    } catch (SQLException err) {
+      System.out.println("Fejl i count err=" + err.getMessage());
+    }
+    return h; //returnerer brugeren til Service
+  }
+
   //VISER ALLE ØNSKERNE PÅ BRUGERENS ØNSKELISTE
   public List<Project> showAll(int id) {
     ArrayList<Project> projects = new ArrayList<>();
     try {
       Connection con = DBManager.getConnection();
-      String SQL = "SELECT * FROM wishwishwish.wishes WHERE user_id = ?"; // Do this line when we know database name and stuff
+      String SQL = "SELECT * FROM projects WHERE user_id = ?"; // Do this line when we know database name and stuff
 
       PreparedStatement ps = con.prepareStatement(SQL);
       ps.setInt(1, id);
@@ -52,10 +80,32 @@ public class ProjectRepository {
 
       while (rs.next()) {
         projects.add(new Project(
-            rs.getInt(2),
+            rs.getString(3),
             rs.getString(4),
-            rs.getString(5),
-            rs.getInt(6)
+            rs.getInt(5)
+        ));
+      }
+    } catch (SQLException ex) {
+      System.out.println(ex.getMessage());
+    }
+    return projects;
+  }
+
+  public List<Project> showAllSubProjects(int id) {
+    ArrayList<Project> projects = new ArrayList<>();
+    try {
+      Connection con = DBManager.getConnection();
+      String SQL = "SELECT * FROM subprojects WHERE project_id = ?"; // Do this line when we know database name and stuff
+
+      PreparedStatement ps = con.prepareStatement(SQL);
+      ps.setInt(1, id);
+      ResultSet rs = ps.executeQuery();
+
+      while (rs.next()) {
+        projects.add(new Project(
+            rs.getString(3),
+            rs.getString(4),
+            rs.getInt(5)
         ));
       }
     } catch (SQLException ex) {
