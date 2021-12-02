762adad51ee355828bee271e07d38db14d462172
Jens-k-m-m <jensmonteiro@gmail.com>
2021-12-02 15:41:12 +0100

    gemmer dags arbjdet

--- src/main/java/com/example/eksamen2021/domain/services/UserService.java
+++ src/main/java/com/example/eksamen2021/domain/services/UserService.java
@@ -8,6 +8,13 @@ import com.example.eksamen2021.repositories.UserRepositoryImpl;
 public class UserService {
 
   UserRepositoryImpl userRepositoryImpl = new UserRepositoryImpl();
+
+
+  public User loginUser(User user)throws UserExceptionMessage { //modtager information (email, password) fra PostController /login -> PostRepository /validateUser
+    return userRepositoryImpl.validateUser(user);
+  }
+
+
   //OBS ny  int# ændert Jens kl.15:03 02-12-2021 vedr.public #int# createUser(User user)
   //før String# ændert Jens kl.15:03 02-12-2021 vedr.public #String# createUser(User user)
   public int createUser(User user) throws UserExceptionMessage {  //DEN HER SKAL LAVES OM, SÅ endpoints kun ligger i Controllers
@@ -24,7 +31,4 @@ public class UserService {
      */
   }
 
-    public User loginUser(User user)throws UserExceptionMessage { //modtager information (email, password) fra PostController /login -> PostRepository /validateUser
-      return userRepositoryImpl.validateUser(user);
-    }
 }
