0136aefe5529204d5edef391a8e8fb63570982ad
Jens-k-m-m <jensmonteiro@gmail.com>
2021-12-10 12:29:45 +0100

    throw new SubprojectErrorMessageException, throw new ProjectErrorMessageException og throw new UserErrorMessageException

--- src/main/java/com/example/eksamen2021/controllers/UserControllerImpl.java
+++ src/main/java/com/example/eksamen2021/controllers/UserControllerImpl.java
@@ -69,7 +69,7 @@ public class UserControllerImpl implements UserController {
   @Override
   public String handleUserError(Model model, Exception exception) {
     model.addAttribute("message", exception.getMessage());
-    return "errorMessageException/user-error-message-exception";
+    return "errorMessageExceptions/user-error-message-exception";
   }
 
   /*@PostMapping("/new-user")
